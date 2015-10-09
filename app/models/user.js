b5db80d7d038a34f9273da6322aaefcc10315ca0
gcampes <gabrieldecampes@gmail.com>
2015-10-09 17:47:09 -0300

    Changed from fixtures to fake-rest

--- app/models/user.js
+++ app/models/user.js
@@ -5,29 +5,4 @@ var User = DS.Model.extend({
   coffeCount: DS.attr('number')
 });
 
-User.reopenClass({
-    FIXTURES: [
-        {
-            id: 1,
-            name: "User 1"
-        },
-        {
-            id: 2,
-            name: "User 2"
-        },
-        {
-            id: 3,
-            name: "User 3"
-        },
-        {
-            id: 4,
-            name: "User 4"
-        },
-        {
-            id: 5,
-            name: "User 5"
-        }
-    ]
-});
-
 export default User;
--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
       'font-src': "'self' https://fonts.gstatic.com https://maxcdn.bootstrapcdn.com",
-      'connect-src': "'self'",
+      'connect-src': "'self' http://localhost:3000",
       'img-src': "'self'",
       'report-uri':"'localhost'",
       'style-src': "'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com https://fonts.googleapis.com/",
